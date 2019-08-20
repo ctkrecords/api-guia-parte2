3c9b94726a048bf25f501ae2b9655538656ebdf8
unknown <ctkrecordings@gmail.com>
2019-08-19 18:51:35 -0600

    ADDED: Format to News Column

--- src/News_column.js
+++ src/News_column.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Content, Columns} from 'react-bulma-components/full';
+import {Content, Columns, Media, Card, Heading, Image} from 'react-bulma-components/full';
 
 const News_column = (props) => {
     if (props.news_col === []){
@@ -9,7 +9,7 @@ const News_column = (props) => {
     }
     return(
         <Columns>
-        <strong>Articles: </strong>
+        <h2>Articles</h2>
             <p>
                 <ul>
                     {
@@ -17,7 +17,33 @@ const News_column = (props) => {
                     }
                 </ul>
             </p>
+            <Card>
+                {
+                    props.news_col.articles.map(news =>  <Card.Image size="3by2" src={news.urlToImage}/>)
+                }    
+               
+                <Card.Content>
+                    <Media>
+                    <Media.Item renderAs="figure" position="left">
+                        <Image renderAs="p" size={64} alt="64x64" src="http://bulma.io/images/placeholders/128x128.png" />
+                    </Media.Item>
+                    <Media.Item>
+                        <Heading size={4}>John Smith</Heading>
+                        <Heading subtitle size={6}>
+                        @johnsmith
+                        </Heading>
+                    </Media.Item>
+                    </Media>
+                    <Content>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#1">#css</a> <a href="#2">#responsive</a>
+                    <br />
+                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    </Content>
+                </Card.Content>
+            </Card>
         </Columns>
+
     );
 
 }
